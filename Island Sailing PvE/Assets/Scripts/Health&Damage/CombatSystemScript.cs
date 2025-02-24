6c76c295490d1462ec4fe347e24d9518dd52f175
Kombu <kayleonhyder@gmail.com>
2025-02-24 12:44:39 -0600

    Starting Mashing mechanic
    - Created Mashing Minigame file
       - Player clicks 1st, and then mashes spacebar

--- Island Sailing PvE/Assets/Scripts/Health&Damage/CombatSystemScript.cs
+++ Island Sailing PvE/Assets/Scripts/Health&Damage/CombatSystemScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0f7a37c9c75d47ba9cb7d5e5bb801535b45ba012b57717aeb676b50ac9914749
-size 10864
+oid sha256:a187dc6a2af7655419b11809ccf720a50b3885c04a7f18fa3293d8dc4c25503a
+size 10752
---
+++ Island Sailing PvE/Assets/Scripts/Minigames/MashingMiniGame.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b7e601ac8298661b6d8eca8ae3cac265954617cd886c93891eed39615c28796f
+size 969
