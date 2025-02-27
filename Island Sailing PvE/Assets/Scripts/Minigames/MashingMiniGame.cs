609c83530203c43c9eccea0dc7d9da0f4071e3e5
Kombu <kayleonhyder@gmail.com>
2025-02-27 12:38:00 -0600

    Mashing Minigame; Finished?
    - Fixed pauses with Coroutine (minigame; under MashingMiniGame)
    - totalSpaceInput is now public
    - Ajusted comments for explanations and to remove old code

--- Island Sailing PvE/Assets/Scripts/Minigames/MashingMiniGame.cs
+++ Island Sailing PvE/Assets/Scripts/Minigames/MashingMiniGame.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:be2a2c4a22f99248668c2b9b41f595104c7eb1d233e1289251a6c57db3fc45b2
-size 2556
+oid sha256:9a4bd8a3aa37744b136bc40ee484719e3a4a3f4a7243d2aa99eca193243ec28d
+size 2839
