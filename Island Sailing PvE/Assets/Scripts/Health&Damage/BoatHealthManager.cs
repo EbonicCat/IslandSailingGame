b52afbafa81b19c46caf3ed077a9c0cb2328d9ee
Kombu <kayleonhyder@gmail.com>
2025-03-28 12:00:58 -0500

    Combat to Sailing Health Fixed
    Fixed health conversion from Combat to Sailing Section (healFromCombat changed to be static)

--- Island Sailing PvE/Assets/Scripts/Health&Damage/BoatHealthManager.cs
+++ Island Sailing PvE/Assets/Scripts/Health&Damage/BoatHealthManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3f4eed1268786123b44bcfb9c6cd6715b6c772b8f362ce5b032cd2411a7d8173
-size 2247
+oid sha256:c01d663ce43b986f577052ea18c8fa17e16e43358e31df8b11dac3299e6a22dc
+size 2254
