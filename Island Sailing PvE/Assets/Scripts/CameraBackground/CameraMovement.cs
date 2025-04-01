e1e21d4a31a290be5642ce7d7388fcd34670cda6
Kombu <kayleonhyder@gmail.com>
2025-04-01 09:10:08 -0500

    Sprites and Camera Fixed
    - Added scrap and flamingo sprites
    - Fixed camera speed

--- Island Sailing PvE/Assets/Scripts/CameraBackground/CameraMovement.cs
+++ Island Sailing PvE/Assets/Scripts/CameraBackground/CameraMovement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a294f718ebb270a0b6cfa1e54835fba4e6ec94af0083fae4c59a2d2e79688502
+oid sha256:2fe7756b3a43669cf8c7ab1f4a610fb486b3bdccbb060c2160337a4e8c062243
 size 639
