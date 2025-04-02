442cc43177e6553dfabd96a0f45c530672f5a2e1
Kombu <kayleonhyder@gmail.com>
2025-04-01 19:33:21 -0500

    Scrap Text Update
    Turns out the scrap conversions worked! Updated the scrap text at the beginning of the sailling section to fix visual problem

--- Island Sailing PvE/Assets/Scripts/Player/PlayerScript.cs
+++ Island Sailing PvE/Assets/Scripts/Player/PlayerScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:45e70bde6e25e31be35cad40d0f4087f692e9b84fbb5fc93f93ca518eb0eee57
-size 3406
+oid sha256:6e8b99aea4a81fa81af7e8ec6ffc1a09faf71b5ce7cae8a290d926758480f1d3
+size 3479
