07859e955ec5f82cc8500c83bd937a38d8534e8a
Kombu <kayleonhyder@gmail.com>
2025-04-07 12:48:12 -0500

    Finished Color Lerping
    Added color lerping to scrap sprites in sailing section
    - white > green
    * Can't add to main menu because scrap is image (not sprite)

--- Island Sailing PvE/Assets/Scripts/Visuals/ColorLerping.cs
+++ Island Sailing PvE/Assets/Scripts/Visuals/ColorLerping.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0f6b695513eeec5f976c78f113731a2c91750484d3bb588f34ee2a4ba076d84f
-size 1057
+oid sha256:d2c4c0d293f48a911b3bc1337f9401bac4f5a606573101d0e3b3fe8ab34688b8
+size 2330
