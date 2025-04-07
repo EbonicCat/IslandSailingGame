d045bb69cca04dd0e09551b123a9abf0e1ad18cf
Kombu <kayleonhyder@gmail.com>
2025-04-07 12:23:46 -0500

    Color Lerping Script
    Created Unique color lerping script (incomplete)

--- Island Sailing PvE/Assets/Scripts/Health&Damage/ScrapItem.cs
+++ Island Sailing PvE/Assets/Scripts/Health&Damage/ScrapItem.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:390d29af4589567ef567db400730c9078d08b6328cb40f3ea5df38bd2872fe61
-size 1630
+oid sha256:7572f9b3eab775df23e3291bad63165fcd09bfb2b14f7ac259ab9adcfb739c71
+size 1252
---
+++ Island Sailing PvE/Assets/Scripts/Visuals/ColorLerping.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0f6b695513eeec5f976c78f113731a2c91750484d3bb588f34ee2a4ba076d84f
+size 1057
