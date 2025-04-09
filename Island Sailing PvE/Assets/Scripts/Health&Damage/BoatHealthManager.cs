d274ab65df4d748543688f4c56c1f3ce5105d5f8
Kombu <kayleonhyder@gmail.com>
2025-04-09 12:32:21 -0500

    Player Color Lerping 1
    - Tried to make player color lerp if health is 20 or less
    - Found issue with health reseting if you die (Sailing 1)

--- Island Sailing PvE/Assets/Scripts/Health&Damage/BoatHealthManager.cs
+++ Island Sailing PvE/Assets/Scripts/Health&Damage/BoatHealthManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d71477685a7768830cd2e34d8deb45744261772da7995c64adb5fe79069294ab
-size 2338
+oid sha256:42a7c8a40bbec9d61e9d2c126d05245c63501e6513006c6d02dd483fbf4226bd
+size 3158
--- Island Sailing PvE/Assets/Scripts/Visuals/ColorLerping.cs
+++ Island Sailing PvE/Assets/Scripts/Visuals/ColorLerping.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d2c4c0d293f48a911b3bc1337f9401bac4f5a606573101d0e3b3fe8ab34688b8
-size 2330
+oid sha256:fe51471a8777c632ffbf2cbeb732a7543ae9c98b746d0d7d07fe968b677f1b25
+size 2865
