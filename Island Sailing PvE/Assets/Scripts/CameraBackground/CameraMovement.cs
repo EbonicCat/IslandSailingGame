c71b32facf66f3fc6e454ffc02e9a9b1b39431cf
Kombu <kayleonhyder@gmail.com>
2025-04-11 12:08:08 -0500

    Save Before Organization
    Starting to organize stuff

--- Island Sailing PvE/Assets/Scripts/CameraBackground/CameraMovement.cs
+++ Island Sailing PvE/Assets/Scripts/CameraBackground/CameraMovement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:867e6ff5822634c5eb061963b0157672dbd3969fb053b23354857a8bfd8f51fa
-size 639
+oid sha256:e1c1869df77da508568acd0cd3cbb190ec2c08215072f53e4d94f2369d7c795b
+size 643
