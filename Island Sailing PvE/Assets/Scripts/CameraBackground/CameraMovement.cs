64379a4aa14c2332e52d358793d31b35e8a96b65
Kombu <kayleonhyder@gmail.com>
2025-04-11 12:16:35 -0500

    Script Updates
    - Removed duplicate scripts from organizing
    - Fixed camera speed

--- Island Sailing PvE/Assets/Scripts/CameraBackground/CameraMovement.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e1c1869df77da508568acd0cd3cbb190ec2c08215072f53e4d94f2369d7c795b
-size 643
---
+++ Island Sailing PvE/Assets/Scripts/Sailing Specific/CameraBackground/CameraMovement.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:96842be7f3984e0355c1c285c50d1bf8cb828381c4b517bdd4d27ab4c03caa3e
+size 643
