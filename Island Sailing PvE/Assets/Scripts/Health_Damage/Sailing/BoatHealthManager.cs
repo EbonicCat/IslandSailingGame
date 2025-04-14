98c5e30803a96757e78b48182ae42f507ebc6f3f
Kombu <kayleonhyder@gmail.com>
2025-04-14 12:33:10 -0500

    Update BoatHealthManager.cs

--- Island Sailing PvE/Assets/Scripts/Health_Damage/Sailing/BoatHealthManager.cs
+++ Island Sailing PvE/Assets/Scripts/Health_Damage/Sailing/BoatHealthManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ea66c5a5b0d41c19a5e2343644fb5dedd41a0b1e809726fe9e1ac5e67873d941
-size 3317
+oid sha256:7f2f03023500b090d6a91eabdcef7a8ad740bbde44b10251c96a6eda34a9e8d2
+size 3425
