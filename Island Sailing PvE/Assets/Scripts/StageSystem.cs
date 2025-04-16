5ca59ad86b387d27f5d41a3391d1206b2431f388
Kombu <kayleonhyder@gmail.com>
2025-04-16 12:41:31 -0500

    Update StageSystem.cs
    - Fixed stage swapping to be shorter and more simple

--- Island Sailing PvE/Assets/Scripts/StageSystem.cs
+++ Island Sailing PvE/Assets/Scripts/StageSystem.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f78f68a30c047816cf3e735143d7151bb0cd8dbf70dd944ed2e01b79debde739
-size 2261
+oid sha256:1d16f22322dae4f2b45989f278e89d4626a6fc4ae29b251bfae242cca2dba681
+size 861
