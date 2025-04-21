2b8df87e4b7cbce123ac0cffb1fdfa908593018c
Kombu <kayleonhyder@gmail.com>
2025-04-21 12:49:37 -0500

    Update SailingTimer.cs
    Worked on Pausing for decision

--- Island Sailing PvE/Assets/Scripts/Time/SailingTimer.cs
+++ Island Sailing PvE/Assets/Scripts/Time/SailingTimer.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd8fc5f2a24c4d3e173f2db3cc344fa9013548d124d61bbf52acfa51b9a0c349
-size 3850
+oid sha256:cf304b930585dd1da833c1fa211628978763b2166545cca2ea49ad7d9924f8aa
+size 4206
