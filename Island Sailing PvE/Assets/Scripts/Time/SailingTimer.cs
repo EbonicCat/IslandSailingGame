8479bdfc9b67b486597050c31c73947f19841664
Kombu <kayleonhyder@gmail.com>
2025-04-21 12:40:32 -0500

    Update SailingTimer.cs
    - Made starting the timer its own method
    - Comments
    - Made methods for path choices and corresponding panel

--- Island Sailing PvE/Assets/Scripts/Time/SailingTimer.cs
+++ Island Sailing PvE/Assets/Scripts/Time/SailingTimer.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:86ee1a76c01ae49cfbbb1bdf05cc73ff59aabed42f6eb769cee01c4cfde914ef
-size 2803
+oid sha256:bd8fc5f2a24c4d3e173f2db3cc344fa9013548d124d61bbf52acfa51b9a0c349
+size 3850
