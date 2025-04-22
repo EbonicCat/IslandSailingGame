f7b7eeed84f4b494d168b6dc212088bfa73f74d2
Kombu <kayleonhyder@gmail.com>
2025-04-22 12:47:11 -0500

    Update Scripts for Testing 2

--- Island Sailing PvE/Assets/Scripts/Health_Damage/Combat/CombatSystemScript.cs
+++ Island Sailing PvE/Assets/Scripts/Health_Damage/Combat/CombatSystemScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0f0a419a5cc0a2b83b34da7e48d3d30d9b1c397aa1044d358feec5ee21ded884
-size 13101
+oid sha256:83c5259c36c533661f8b0eeaef4d93914d7bb8d931e6e3f0b4dc0ffa5be02e3b
+size 13107
--- Island Sailing PvE/Assets/Scripts/Time/SailingTimer.cs
+++ Island Sailing PvE/Assets/Scripts/Time/SailingTimer.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f94e13599fb4911e331f771d377405c1519c03e818dad1ea80d6e3f0952c4ec6
-size 3946
+oid sha256:d3937d8730702077384828f8acfaf889ad7500ee8fecc6209581420c6bb85d51
+size 4015
