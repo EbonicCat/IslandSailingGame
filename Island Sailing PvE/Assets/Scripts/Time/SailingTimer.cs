1a7a54b49f6187db027ea27b52bfa545321a23c6
Kombu <kayleonhyder@gmail.com>
2025-04-23 12:49:32 -0500

    Update SailingTimer.cs
     Commented Out Logs

--- Island Sailing PvE/Assets/Scripts/Time/SailingTimer.cs
+++ Island Sailing PvE/Assets/Scripts/Time/SailingTimer.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:02acc9c30fdbe9318a4ec8755fa352f3a16337531a85f6277372e9e149958f47
-size 5019
+oid sha256:b9695f6b16b3c350bd5ae4fe2d4ff77356da62a1447a558f659f1e4c8db16a30
+size 5027
