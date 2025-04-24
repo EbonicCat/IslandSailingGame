7f60722214e39e2b81895e53c20f7bafe811a3f3
Kombu <kayleonhyder@gmail.com>
2025-04-24 12:14:29 -0500

    Update StageSystemCombat.cs
    - Added Dictionary with enemy order for each stage
    - Added GetStage method

--- Island Sailing PvE/Assets/Scripts/StageSystemCombat.cs
+++ Island Sailing PvE/Assets/Scripts/StageSystemCombat.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0ce540d6e25c02e6c40816df2db457135070f5f190fd2aa8ce3b371964fe6561
-size 955
+oid sha256:b2503b90c4175f2f77ebdad6e42af197705f1a9c362161b27cc4f46b52d2b5d8
+size 1645
