94dde091cc19d583bf1af64db4515c4705a99d26
Kombu <kayleonhyder@gmail.com>
2025-04-25 12:26:04 -0500

    MoveHUD & Round-End Rework
    - Reworked MoveHUD to be based on enemy type instead of roundNum, and made move locations custom (within Unity itself
    - Reworked ending the round to be based on "stop" string in EnemyOrder array

--- Island Sailing PvE/Assets/Scripts/Health_Damage/Combat/CombatSystemScript.cs
+++ Island Sailing PvE/Assets/Scripts/Health_Damage/Combat/CombatSystemScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6bb700d12f65ab0296a7812a9c58183beb8feebfc75af947b9b0a8344d77d4a2
-size 14620
+oid sha256:26fff04825d4d3c7fbd86466dc6b8811dade32ce272e986ca9ef06a059bbd317
+size 14787
