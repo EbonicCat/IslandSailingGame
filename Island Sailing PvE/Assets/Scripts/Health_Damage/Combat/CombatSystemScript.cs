db3605785a6455a57235b1d9d45f07f8367e9f40
Kombu <kayleonhyder@gmail.com>
2025-05-02 12:15:52 -0500

    Fork In Road Work Final
    - Made it to where the fork in road actually appears at the right time (after combat 2)
    - DIDN'T fix the buttons

--- Island Sailing PvE/Assets/Scripts/Health_Damage/Combat/CombatSystemScript.cs
+++ Island Sailing PvE/Assets/Scripts/Health_Damage/Combat/CombatSystemScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6d151b52ed520b30cc43a2d0a0fef8dbd7876e334db08571bcb4305cef0d3bad
-size 15556
+oid sha256:ad92f14bbe2fe3d3619026a2c4d6c73613bf1f223ff716b89cad8b65d9612acb
+size 15568
--- Island Sailing PvE/Assets/Scripts/Time/SailingTimer.cs
+++ Island Sailing PvE/Assets/Scripts/Time/SailingTimer.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:62480ef376dc511e67e453b01faf4d6f1d41c3ecdd34cc3e00ad7d029b974d26
-size 5308
+oid sha256:8eb0b189ef9aa588c215f3830b80a48492e880d4acaffed751e1d04a82a8dbe5
+size 5057
