312eb8c4bdb444d4a7131a70a220573ee6c93490
Kombu <kayleonhyder@gmail.com>
2025-05-02 12:20:52 -0500

    Fix Fork in Road Buttons

--- Island Sailing PvE/Assets/Scripts/StageSystemSailing.cs
+++ Island Sailing PvE/Assets/Scripts/StageSystemSailing.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6e2c308ba9f0aa8027e3ebd0bce4a8f60e31072a2b469525c44584a51d71ce15
-size 2324
+oid sha256:15a724dfdce767c44177145f4c8f5088aab703c30c9e2a122ab2e5dcd6ba3372
+size 2213
