9ecbaa04351a1f44618ecd3ff329d69c772d5a18
Kombu <kayleonhyder@gmail.com>
2025-05-02 12:37:32 -0500

    Scrap Display Error Fix
    Fixed problem where the scraps displayed 0 in sailing stages past 1 until the player used/collected scraps

--- Island Sailing PvE/Assets/Scripts/Sailing Specific/PlayerScript.cs
+++ Island Sailing PvE/Assets/Scripts/Sailing Specific/PlayerScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:647b1a3949941940f995bcd7999a9186285fe075f6f6f5545a2f9feb2d62cc89
-size 3837
+oid sha256:d1b2ca0999d79e6efd0640e20834f31b0ad11429985ddd485879d2346dcc9989
+size 3850
