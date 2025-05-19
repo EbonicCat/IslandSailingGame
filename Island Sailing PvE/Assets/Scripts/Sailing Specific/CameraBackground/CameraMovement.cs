d4c80146cc0c79639c254d655187d2e3b48a6631
Kombu <kayleonhyder@gmail.com>
2025-05-18 21:40:44 -0500

    Speed Fixes

--- Island Sailing PvE/Assets/Scripts/Sailing Specific/CameraBackground/CameraMovement.cs
+++ Island Sailing PvE/Assets/Scripts/Sailing Specific/CameraBackground/CameraMovement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cda22c7202a907d99c089f85b1f9ff0babefa916ac4d5cdaf23f2021614987a9
-size 643
+oid sha256:a95610553d9d36523e77041cad9eba5d7758201a07d19cdf13468710126e9516
+size 412
