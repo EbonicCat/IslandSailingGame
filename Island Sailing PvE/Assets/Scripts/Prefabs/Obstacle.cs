621d16f168df785a48d2776764f520e107444dcd
Kombu <kayleonhyder@gmail.com>
2025-05-27 13:55:24 -0500

    Tag Collision Destory for Build

--- Island Sailing PvE/Assets/Scripts/Prefabs/Obstacle.cs
+++ Island Sailing PvE/Assets/Scripts/Prefabs/Obstacle.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6729392e140f103273bc26e5853305d1ab4973f99b3de0eb4ad9fb309243adf0
-size 1460
+oid sha256:0e6737418c4a96735b3b6e1731d24d53f5b9a9fb9d14db881de1d5f6ba0eaabb
+size 1491
--- Island Sailing PvE/Assets/Scripts/Prefabs/ScrapItem.cs
+++ Island Sailing PvE/Assets/Scripts/Prefabs/ScrapItem.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7572f9b3eab775df23e3291bad63165fcd09bfb2b14f7ac259ab9adcfb739c71
-size 1252
+oid sha256:c73c9832015c1b3c223f8bcd2e3ef5e55cc74a54a8f54e78a33264dfac3e4ca4
+size 1279
